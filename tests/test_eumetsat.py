bbfcf1035ca600f189f2c97e170e6b9b306eb864
AyrtonB <AyrtonBourn@Outlook.com>
2020-10-23 09:58:12 +0100

    Update test_eumetsat.py

--- tests/test_eumetsat.py
+++ tests/test_eumetsat.py
@@ -2,12 +2,14 @@ import os
 import pytest
 import dotenv
 
-print(os.listdir())
-dotenv.load_dotenv('../.env')
+def test_load_env_vars():
+    print(os.listdir())
+    dotenv.load_dotenv('../.env')
 
-user_key = os.environ.get('user_key')
-user_secret = os.environ.get('user_secret')
-slack_id = os.environ.get('slack_id')
-slack_webhook_url = os.environ.get('slack_webhook_url')
+    user_key = os.environ.get('user_key')
+    user_secret = os.environ.get('user_secret')
+    slack_id = os.environ.get('slack_id')
+    slack_webhook_url = os.environ.get('slack_webhook_url')
 
-print(slack_id)
+    assert user_key is not None
+    print(slack_id)
