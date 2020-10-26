10268854e9aa2e2964c788ae8566222ba858afc0
AyrtonB <AyrtonBourn@Outlook.com>
2020-10-26 20:05:21 +0000

    added pyinterp transformation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("satip/README.md", "r") as fh:
 
 setuptools.setup(
     name="satip",
-    version="0.0.2",
+    version="0.0.3",
     author="Ayrton Bourn",
     author_email="ayrton@futureenergy.associates",
     description="Satip is a Python library for satellite image processing",
